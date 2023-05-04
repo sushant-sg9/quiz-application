e138ba784c9848c80ea7c68d1b5a5ab2e2581994
sushant-sg9 <sushantghadge2016@gmail.com>
2023-05-04 19:36:19 +0530

    quiz-application

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the following is not javascript data types?",
+    answer: "All of the mentioned",
+    options: [
+       "Null type",
+       "Undefined type",
+       "Number type",
+       "All of the mentioned"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+    answer: "Window",
+    options: [
+      "Position",
+      "Window",
+      "Standard",
+      "Location"
+    ]
+  },
+  {
+    numb: 8,
+    question: "A program in HTML can be rendered and read by -",
+    answer: "Web browsers",
+    options: [
+      "Web browsers",
+      "Interpreter",
+      "Server",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: " Which of the following are the attributes of the tag?",
+    answer: "Both (a) & (b)",
+    options: [
+      "method",
+      "action",
+      "None of the above",
+      "Both (a) & (b)"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following attribute is used to provide a unique name to an element?",
+    answer: "id",
+    options: [
+      "id",
+      "class",
+      "type",
+      "None of the above"
+    ]
+  }
+];
